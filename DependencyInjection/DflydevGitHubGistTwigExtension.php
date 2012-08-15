115d9f3a2f14c9a90684f6d5d5b05e0a219cbb6f
Beau Simensen <beau@dflydev.com>
2012-08-15 07:35:51 -0700

    dflydev/github-gist-twig-extension 2.x

--- DependencyInjection/DflydevGitHubGistTwigExtension.php
+++ DependencyInjection/DflydevGitHubGistTwigExtension.php
@@ -38,7 +38,7 @@ class DflydevGitHubGistTwigExtension extends Extension
         if (isset($config['cache']) && isset($config['cache']['enabled']) && $config['cache']['enabled']) {
             $container
                 ->findDefinition('dflydev_twig_github_gist.extension')
-                ->addArgument(new Reference('dflydev_twig_github_gist.cache.filesystem'));
+                ->addMethodCall('setCache', array(new Reference('dflydev_twig_github_gist.cache.filesystem')));
         }
     }
 }
